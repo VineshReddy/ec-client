1471c0b2e17e4acaa55a5f773883ce3101f90bed
Vinesh Reddy <vineshreddy1997@gmail.com>
2021-01-03 15:57:13 +0530

    abandoned

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,23 @@ import React, {useState, useEffect} from 'react'
 
 const HomePage = () => {
   return (
-    <main className="flex-center rem15">
-      A Project By Vinesh Reddy
+    <main >
+      <img src="https://i.gifer.com/YVPE.gif" width="300px" alt="" />
+      <p>This project is abandoned in May 2019.</p>
+      <p><b>Features: </b></p>
+      <ul>
+        <li>Categories and SubCategories</li>
+        <li>Account</li>
+        <li>Cart</li>
+        <li>Orders</li>
+        <li>Search (Need to be improved)</li>
+      </ul>
+      <p><b>Demo:</b></p>
+      <ul>
+        <li>A single Category(<em>Groceries</em>) is implemented with 2 SubCategories(<em>Pluses, Flours</em>) just for example but could have easily implement all categories without extra work.</li>
+        <li>User can order Products and specify the quantity.</li>
+        <li>Checkout page not implemented.</li>
+      </ul>
     </main>
 
   )
