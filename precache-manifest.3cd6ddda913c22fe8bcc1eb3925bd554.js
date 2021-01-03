22937854440c32bc260f7e8bebb58dd106122102
Vinesh Reddy <vineshreddy1997@gmail.com>
2021-01-03 18:21:42 +0530

    Updates

---
+++ precache-manifest.3cd6ddda913c22fe8bcc1eb3925bd554.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883c6c5d1101e4ffb4e854b6e53460b9",
+    "url": "/ec-client/index.html"
+  },
+  {
+    "revision": "847d0a5b113ff179a7b0",
+    "url": "/ec-client/static/css/3.107ca6ad.chunk.css"
+  },
+  {
+    "revision": "75e664d0a0bc2afe0268",
+    "url": "/ec-client/static/css/main.c631c575.chunk.css"
+  },
+  {
+    "revision": "2fbe776735741ee46572",
+    "url": "/ec-client/static/js/0.fc904505.chunk.js"
+  },
+  {
+    "revision": "847d0a5b113ff179a7b0",
+    "url": "/ec-client/static/js/3.9badaaa2.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/ec-client/static/js/3.9badaaa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1329320bf83af6a79d1d",
+    "url": "/ec-client/static/js/4.ed5c4b6c.chunk.js"
+  },
+  {
+    "revision": "3b485466ed8bf7041b2a",
+    "url": "/ec-client/static/js/5.9dd3b417.chunk.js"
+  },
+  {
+    "revision": "75e664d0a0bc2afe0268",
+    "url": "/ec-client/static/js/main.d6430e86.chunk.js"
+  },
+  {
+    "revision": "454081e329c849f64fb7",
+    "url": "/ec-client/static/js/runtime-main.6ef128a7.js"
+  }
+]);
