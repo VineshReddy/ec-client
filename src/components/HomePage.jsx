cdde99aba3b2e379d0a112e54ec46c60f27a7db6
Vinesh Reddy <vineshreddy1997@gmail.com>
2021-01-03 17:37:03 +0530

    proxy changed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -18,6 +18,7 @@ const HomePage = () => {
         <li>A single Category(<em>Groceries</em>) is implemented with 2 SubCategories(<em>Pluses, Flours</em>) just for example but could have easily implement all categories without extra work.</li>
         <li>User can order Products and specify the quantity.</li>
         <li>Checkout page not implemented.</li>
+        <li>Demo User: test@gmail.com Password: test</li>
       </ul>
     </main>
 
